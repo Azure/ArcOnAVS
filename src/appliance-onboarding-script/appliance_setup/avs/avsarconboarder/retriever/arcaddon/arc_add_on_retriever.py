274c2a8018f711738e416e90678a0995d56d51bc
Abhilash Jakka <venkatjakka@microsoft.com>
2022-11-03 16:20:05 +0530

    renaming arc_add_on to add_on

--- src/appliance-onboarding-script/appliance_setup/avs/avsarconboarder/retriever/arcaddon/arc_add_on_retriever.py
+++ src/appliance-onboarding-script/appliance_setup/avs/avsarconboarder/retriever/arcaddon/arc_add_on_retriever.py
@@ -11,7 +11,7 @@ from ...retriever._retriever import Retriever
 
 class ArcAddOnRetriever(Retriever):
     instance = None
-    _arc_add_on_url = Constant.MGMT_URL + "addons" + "?" + Constant.API_VERSION + "=" + Constant.PREVIEW_API_VERSION_VALUE
+    _add_on_url = Constant.MGMT_URL + "addons" + "?" + Constant.API_VERSION + "=" + Constant.PREVIEW_API_VERSION_VALUE
     
     def __new__(cls):
         if cls.instance is None:
@@ -21,12 +21,12 @@ class ArcAddOnRetriever(Retriever):
 
     def retrieve_data(self, customer_resource):
         customer_res: CustomerResource = customer_resource
-        arc_add_on_url = self._arc_add_on_url.format(customer_res.subscription_id,
-                                                  customer_res.resource_group,
-                                                  customer_res.private_cloud)
+        add_on_url = self._add_on_url.format(customer_res.subscription_id,
+                                                customer_res.resource_group,
+                                                customer_res.private_cloud)
 
         az_cli = AzCli().append(Constant.REST).append("-m").append(Constant.GET).append("-u"). \
-            append(arc_add_on_url)
+            append(add_on_url)
 
         res = self._az_cli_executor.run_az_cli(az_cli)
     
